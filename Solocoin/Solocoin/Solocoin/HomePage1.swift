575fae8e0dfa93fa24fde68b5da2ae744702cc95
ecomparer <mishaalhere@gmail.com>
2020-07-24 00:47:07 +0300

    changes,confetti

--- Solocoin/Solocoin/Solocoin/HomePage1.swift
+++ Solocoin/Solocoin/Solocoin/HomePage1.swift
@@ -398,7 +398,6 @@ class HomePage1: UIViewController, CLLocationManagerDelegate, GLScratchCardDeleg
             return
         }
         request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         let content = ["token": notiKey]
         print("noti",content)
         let jsonEncoder = JSONEncoder()
